4e0465faf91009b09bb2783ae6315b157aea8c50
ClausWiingreen <claus@wiingreen.dk>
2014-07-07 23:27:01 +0200

    Ground Work
    Added the main functionality of the tile and board classes.

---
+++ Gamefiles/Assets/Scripts/Board.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Board : MonoBehaviour, IEnumerable<Tile>
+{
+    Tile[] _tiles;
+
+    public GameObject TilePrefab;
+
+    void Awake()
+    {
+        _tiles = new Tile[64];
+        for (int i = 0; i < 64; i++)
+        {
+            GameObject go = Instantiate(TilePrefab) as GameObject; //GameObject.CreatePrimitive(PrimitiveType.Cube);
+            go.name = "Tile" + i;
+
+            var relativePosition = new Vector3(i % 8, -(i / 8));
+
+            if((i/8)%2 != 0)
+            {
+                relativePosition.x += 0.5f;
+            }
+
+            go.transform.position = transform.position + relativePosition;
+            go.transform.parent = this.transform;
+
+            _tiles[i] = go.AddComponent<Tile>();
+
+            if(i%8 > 0)
+            {
+                _tiles[i].AddNeighbor(_tiles[i-1]);
+            }
+            if(i >= 8)
+            {
+                _tiles[i].AddNeighbor(_tiles[i-8]);
+                if ((i / 8) % 2 == 0 && i % 8 > 0) 
+                    _tiles[i].AddNeighbor(_tiles[i - 9]);
+                else if((i / 8) % 2 != 0 && i % 8 < 7)
+                    _tiles[i].AddNeighbor(_tiles[i - 7]);
+            }
+        }
+    }
+
+    #region IEnumerable<Tile>
+    public IEnumerator<Tile> GetEnumerator()
+    {
+        int i = _tiles.Length;
+        while (i != 0)
+        {
+            yield return _tiles[i];
+            i--;
+        }
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator() as IEnumerator;
+    }
+    #endregion
+}
---
+++ Gamefiles/Assets/Scripts/Tile.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+using System.Linq;
+
+public class Tile : MonoBehaviour, IEquatable<Tile>
+{
+    /// <summary>
+    ///     The neighbors of this tile
+    /// </summary>
+    private List<Tile> Neighbors = new List<Tile>();
+
+    void OnMouseEnter()
+    {
+        HighLight();
+        Neighbors.ForEach(n => n.HighLight());
+    }
+
+    void OnMouseExit()
+    {
+        DeHighLight();
+        Neighbors.ForEach(n => n.DeHighLight());
+    }
+
+    void OnMouseDown()
+    {
+        StartCoroutine(Wave());
+    }
+
+    IEnumerator Wave()
+    {
+        List<Tile> waved = new List<Tile>();
+        List<Tile> selected = new List<Tile>{this};
+
+        do
+        {
+            selected.ForEach(t => t.DeHighLight());
+            selected = selected.SelectMany(t => t.Neighbors.Where(n => !waved.Contains(n) && n.gameObject.activeInHierarchy)).ToList();
+            selected.ForEach(t => t.HighLight());
+            waved.AddRange(selected);
+            yield return new WaitForSeconds(.1f);
+        } while (selected.Count != 0);
+    }
+
+    /// <summary>
+    ///     Adds a connection between the tiles.
+    /// </summary>
+    /// <param name="neighbor">The neighbor.</param>
+    public void AddNeighbor(Tile neighbor)
+    {
+        Neighbors.Add(neighbor);
+        neighbor.Neighbors.Add(this);
+    }
+
+    void HighLight()
+    {
+        renderer.material.color = Color.cyan;
+    }
+    void DeHighLight()
+    {
+        renderer.material.color = Color.white;
+    }
+
+    /// <summary>
+    ///     Indicates whether the current tile is equal to another tile.
+    /// </summary>
+    /// <param name="other">A tile to compare with this tile.</param>
+    /// <returns>
+    ///     True if the current tile is equal to the <paramref name="other" /> parameter; otherwise, false.
+    /// </returns>
+    public bool Equals(Tile other)
+    {
+        if(ReferenceEquals(other, null))
+            return false;
+        if(ReferenceEquals(this, other))
+            return true;
+        
+        return false;
+    }
+}
